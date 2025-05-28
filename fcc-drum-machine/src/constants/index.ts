58b032db95179de7d17da92c3b17bef367ed9958
mpsalunggg <putrasatria893@gmail.com>
2025-05-28 13:08:13 +0700

    feat: setup global style, data example

---
+++ fcc-drum-machine/src/constants/index.ts
@@ -0,0 +1,14 @@
+const data = [
+  {
+    keyTrigger: 'Q',
+    audio:
+      'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3',
+    title: 'Heater 1',
+  },
+  {
+    keyTrigger: 'W',
+    audio:
+      'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3',
+    title: 'Heater 2',
+  },
+]
